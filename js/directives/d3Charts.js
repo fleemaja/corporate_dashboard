0ee5053273c909c645010f45d1e1f92b01bcb264
Drew Fleeman <drew.fleeman@gmail.com>
2016-12-07 20:24:34 -0600

    line chart responsively loads based on container width

--- js/directives/d3Charts.js
+++ js/directives/d3Charts.js
@@ -4,81 +4,81 @@
   'use strict';
 
   angular.module('corporateDashboard.directives')
-    .directive('d3Bars', ['d3', function(d3) {
-      return {
-        restrict: 'EA',
-        scope: {
-          data: "=",
-          label: "@",
-          onClick: "&"
-        },
-        link: function(scope, iElement, iAttrs) {
-          var svg = d3.select(iElement[0])
-              .append("svg")
-              .attr("width", "100%");
+  .directive('d3Bars', ['d3', function(d3) {
+    return {
+      restrict: 'EA',
+      scope: {
+        data: "=",
+        label: "@",
+        onClick: "&"
+      },
+      link: function(scope, iElement, iAttrs) {
+        var svg = d3.select(iElement[0])
+            .append("svg")
+            .attr("width", "100%");
 
-          // on window resize, re-render d3 canvas
-          window.onresize = function() {
-            return scope.$apply();
-          };
-          scope.$watch(function(){
-              return angular.element(window)[0].innerWidth;
-            }, function(){
-              return scope.render(scope.data);
-            }
-          );
+        // on window resize, re-render d3 canvas
+        window.onresize = function() {
+          return scope.$apply();
+        };
+        scope.$watch(function(){
+            return angular.element(window)[0].innerWidth;
+          }, function(){
+            return scope.render(scope.data);
+          }
+        );
 
-          // watch for data changes and re-render
-          scope.$watch('data', function(newVals, oldVals) {
-            return scope.render(newVals);
-          }, true);
+        // watch for data changes and re-render
+        scope.$watch('data', function(newVals, oldVals) {
+          return scope.render(newVals);
+        }, true);
 
-          // define render function
-          scope.render = function(data){
+        // define render function
+        scope.render = function(data){
 
-            // setup variables
-            var width, height, max;
-            width = d3.select(iElement[0])[0][0].offsetWidth - 20;
-              // 20 is for margins and can be changed
-            height = scope.data.length * 35;
-              // 35 = 30(bar height) + 5(margin between bars)
-            max = 98;
-              // this can also be found dynamically when the data is not static
-              // max = Math.max.apply(Math, _.map(data, ((val)-> val.count)))
+          // setup variables
+          var width, height, max;
+          width = d3.select(iElement[0])[0][0].offsetWidth - 20;
+            // 20 is for margins and can be changed
+          height = scope.data.length * 35;
+            // 35 = 30(bar height) + 5(margin between bars)
+          max = 98;
+            // this can also be found dynamically when the data is not static
+            // max = Math.max.apply(Math, _.map(data, ((val)-> val.count)))
 
-            // set the height based on the calculations above
-            svg.attr('height', height);
+          // set the height based on the calculations above
+          svg.attr('height', height);
 
-            //create the rectangles for the bar chart
-            svg.selectAll("rect")
-              .data(data)
-              .enter()
-                .append("rect")
-                .attr("height", 30) // height of each bar
-                .attr("width", 0) // initial width of 0 for transition
-                .attr("x", 10) // half of the 20 side margin specified above
-                .attr("y", function(d, i){
-                  return i * 35;
-                }) // height + margin between bars
-                .transition()
-                  .duration(1000) // time of duration
-                  .attr("width", function(d){
-                    return d.score/(max/width);
-                  }); // width based on scale
+          //create the rectangles for the bar chart
+          svg.selectAll("rect")
+            .data(data)
+            .enter()
+              .append("rect")
+              .attr("height", 30) // height of each bar
+              .attr("width", 0) // initial width of 0 for transition
+              .attr("x", 10) // half of the 20 side margin specified above
+              .attr("y", function(d, i){
+                return i * 35;
+              }) // height + margin between bars
+              .transition()
+                .duration(1000) // time of duration
+                .attr("width", function(d){
+                  return d.score/(max/width);
+                }); // width based on scale
 
-            svg.selectAll("text")
-              .data(data)
-              .enter()
-                .append("text")
-                .attr("fill", "#fff")
-                .attr("y", function(d, i){return i * 35 + 22;})
-                .attr("x", 15)
-                .text(function(d){return d.title;});
+          svg.selectAll("text")
+            .data(data)
+            .enter()
+              .append("text")
+              .attr("fill", "#fff")
+              .attr("y", function(d, i){return i * 35 + 22;})
+              .attr("x", 15)
+              .text(function(d){return d.title;});
 
-          };
-        }
-      };
-    }])
+        };
+      }
+    };
+  }])
     .directive('d3Lines', ['d3', function(d3) {
       return {
         restrict: 'EA',
@@ -89,9 +89,10 @@
         },
         link: function(scope, iElement, iAttrs) {
           // Set the dimensions of the canvas / graph
-          var margin = {top: 30, right: 20, bottom: 30, left: 50},
-              width = 600 - margin.left - margin.right,
-              height = 270 - margin.top - margin.bottom;
+          var margin = {top: 30, right: 40, bottom: 30, left: 40},
+              width = parseInt(d3.select('#charts').style('width')) - margin.left - margin.right,
+              mapRatio = 0.5,
+              height = (width * mapRatio) - margin.top - margin.bottom;
           // Adds the svg canvas
           var svg = d3.select(iElement[0])
               .append("svg")
